b43f177bb1c1e851c1384ce23222207adb1608ae
dhebarp <paritoh.dhebar@gmail.com>
2020-05-22 09:26:05 +1000

    server running with nodemon

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("port" + port);
+});
