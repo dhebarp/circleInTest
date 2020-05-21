6b0531f053893fcd7129063f558a9733b82fbf2b
dhebarp <paritoh.dhebar@gmail.com>
2020-05-22 09:47:03 +1000

    updated puppeteer function to include keyword param

--- backend/src/models/puppeteer.js
+++ backend/src/models/puppeteer.js
@@ -1,18 +1,19 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function scrapeUrl(url) {
-
+async function scrapeUrl(url, keyWord) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
-  const contents = await page.$eval('body', el => el.innerText)
+  const contents = await page.$eval("body", (el) => el.innerText);
 
+  const reg = new RegExp(keyWord, "gi");
 
+  const matched = contents.match(reg).length;
 
   browser.close();
-console.log(contents);
-  return contents
+  console.log(matched);
+  return matched;
 }
 
-scrapeUrl('https://en.wikipedia.org/wiki/Abraham_Lincoln#Funeral_and_burial')
+scrapeUrl("https://en.wikipedia.org/wiki/Abraham_Lincoln#Funeral_and_burial", "Abraham");
