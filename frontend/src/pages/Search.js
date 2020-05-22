1f2259f7d105d314ef04541e328345a318a17d6e
dhebarp <paritoh.dhebar@gmail.com>
2020-05-22 13:05:29 +1000

    setup basic form

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 export const Search = () => {
+
+  const [keyWord, setkeyWord] = useState("");
+  const [url, setUrl] = useState("");
+  const [results, setResults] = useState("");
+  const [spinner, setSpinner] = useState("");
+  const [conditional, setConditional] = useState(false);
+
   return (
-    <div>
-      <p> Search Here</p>
-    </div>
+   <>
+     <div className="container">
+        <Form onSubmit={submitHandler}>
+          <Form.Group>
+            <Form.Control
+              required="true"
+              value={keyWord}
+              placeholder="Enter keyword"
+              onChange={(e) => setkeyWord(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              required="true"
+              value={url}
+              placeholder="Enter URL  e.g. https://www.google.com"
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </Form.Group>
+          <Button type="submit">Search</Button>
+        </Form>
+      </div>
+   </>
   )
 }
