d7b1c0a776f4fe051d9665ad65766c8d24f46980
dhebarp <paritoh.dhebar@gmail.com>
2020-05-22 13:15:17 +1000

    created submit function for POST and displayed results

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -9,10 +9,38 @@ export const Search = () => {
   const [spinner, setSpinner] = useState("");
   const [conditional, setConditional] = useState(false);
 
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    fetch("search/v1", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "same-origin",
+      body: JSON.stringify({
+        keyWord: keyWord,
+        channelURL: url,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data:", data);
+        setResults(data.data);
+        setUrl("");
+        setkeyWord("");
+        setConditional(true);
+      });
+  };
+
+
   return (
    <>
      <div className="container">
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group>
             <Form.Control
               required="true"
@@ -31,6 +59,9 @@ export const Search = () => {
           </Form.Group>
           <Button type="submit">Search</Button>
         </Form>
+        {results.keyWord}
+        {results.value}
+        {results.url}
       </div>
    </>
   )
